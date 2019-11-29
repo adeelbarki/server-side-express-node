a68d2dcdfc3b682b9fdae2919f0ff33c28d36c98
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-11-29 16:31:13 +0500

    Added generate_uid api in routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var users = require('./routes/users');
+var generate_uid = require('./routes/generate_uid');
 
 var app = express();
 
@@ -12,5 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
 app.use('/api/v1/users', users);
+app.use('/api/v1/generate_uid', generate_uid)
 
 module.exports = app;
