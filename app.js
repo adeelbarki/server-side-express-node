c1047f47673a334abadb99dd89d09893a755d615
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-12-04 13:38:45 +0500

    Updated customer in router and connected app to router

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var users = require('./routes/users');
+var customer = require('./routes/customer');
 var generate_uid = require('./routes/generate_uid');
 
 var app = express();
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+app.use('/api/v1/customer', customer);
 app.use('/api/v1/generate_uid', generate_uid)
 
 module.exports = app;
